5b5969f33231e2302f68d552c12a3649546fc7df
Matt Keys <MatthewjKeys22@gmail.com>
2022-05-14 16:00:23 -0600

    last commit got messed up, added in no more tours section and a refresh button

--- setup/src/App.js
+++ setup/src/App.js
@@ -5,6 +5,14 @@ import Tours from "./Tours";
 // I SWITCHED TO PERMANENT DOMAIN
 const url = "https://course-api.com/react-tours-project";
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour)=> tour.id !== id);
+    setTours(newTours)
+  }
+
   const fetchTours = async () => {
     setLoading(true);
 
@@ -24,17 +32,21 @@ function App() {
   }, []);
 
   if (loading) {
-    return (
-      <main>
-        <Loading />
-      </main>
-    );
-  }
-  return (
-    <main>
-      <Tours tours={tours} removeTour={removeTour} />
+        return <main>
+          <Loading />
+        </main>
+      } 
+  if (tours.length == 0){
+    return <main>
+      <div className="title">
+    <h2>No Tours Left</h2>
+    <button className="btn" onClick={()=> fetchTours()}>Refresh</button>
+      </div>
     </main>
-  );
+  }
+  return <main>
+          <Tours tours={tours} removeTour={removeTour} />
+        </main>
 }
 
 export default App;
