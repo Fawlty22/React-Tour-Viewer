139d4bd819b3c7bca31e77c63fcc66dc56b8943e
Matt Keys <MatthewjKeys22@gmail.com>
2022-05-14 15:52:47 -0600

    initial commit with react app built and css included

---
+++ setup/src/Tour.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+const Tour = ({ id, image, info, price, name, removeTour }) => {
+  const [readMore, setReadMore] = useState(false);
+
+  return (
+    
+  );
+};
+
+export default Tour;
---
+++ setup/src/Tours.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Tour from './Tour';
+const Tours = ({tours, removeTour}) => {
+  return <section>
+    
+
+  </section>;
+};
+
+export default Tours;
