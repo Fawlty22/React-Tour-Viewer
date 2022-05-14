24b145ae5ddb7a7fdf7bc99d16886d65cd90d963
Matt Keys <MatthewjKeys22@gmail.com>
2022-05-14 15:56:44 -0600

    tours component built as main section with section mapping over tour array

--- setup/src/Tours.js
+++ setup/src/Tours.js
@@ -2,7 +2,16 @@ import React from 'react';
 import Tour from './Tour';
 const Tours = ({tours, removeTour}) => {
   return <section>
-    
+    <div className='title'>
+      <h2>Our Tours</h2>
+      <div className='underline'>
+      </div>
+    </div>
+    <div>
+    {tours.map((tour) => {
+      return <Tour key={tour.id} removeTour={removeTour} {...tour} />
+    })}
+    </div>
 
   </section>;
 };
